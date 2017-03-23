ba6c268eb32078ca5281009ec965263862b6ed8d
Olli-Pekka Oras <Olli-Pekka.Oras@conmio.com>
2017-03-23 15:39:54 +0200

    FoodTracker tutorial 1.0

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by ooras on 22/03/2017.
+//  Copyright © 2017 Jooh. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
